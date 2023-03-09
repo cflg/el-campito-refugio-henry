d9c623dcf022793943b154dd8ccf28c0376cdd90
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-03-09 20:36:24 -0300

    Cambie la url de la db para subir el back a produccion

--- api/config/mongo.js
+++ api/config/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
-  const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.z20k9qo.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`;
+  /* const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.z20k9qo.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`; */
+  const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@elcampito.icl7az8.mongodb.net/?retryWrites=true&w=majority`
 
   // const DB_URI = "mongodb://localhost:27017/MyDB";
   return mongoose.connect(
