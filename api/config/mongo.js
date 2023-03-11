943efd4a90a4f9609d5e6881087ffe0655bbbcfa
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-03-10 23:00:19 -0300

    acceso a la db y a mercadopago

--- api/config/mongo.js
+++ api/config/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = () => {
   /* const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.z20k9qo.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`; */
-  const DB_URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@elcampito.icl7az8.mongodb.net/?retryWrites=true&w=majority`
+  const DB_URI = "mongodb://mongo:eyce93TF6TMHuaYDmo6r@containers-us-west-193.railway.app:6952"
 
   // const DB_URI = "mongodb://localhost:27017/MyDB";
   return mongoose.connect(
